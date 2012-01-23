92a0fbf56d1bfd3cf05d407e6cb7e7e0991ce270
Mateusz Machalica <mateuszmachalica@gmail.com>
2012-01-23 18:14:15 +0100

    Better multi tests.

--- tests/multi.c
+++ tests/multi.c
@@ -8,6 +8,8 @@
 #define CYCLES 10000000
 #define PROCNUM 20
 
+#define TRY(code) if (code) {printf("ERROR\n"); return 1;}
+
 int main(int argc, char** argv)
 {
 	int shmid;
@@ -19,29 +21,29 @@ int main(int argc, char** argv)
 	pid_t pid;
 
 	shmid = shmget(IPC_PRIVATE, sizeof(futex_t) + sizeof(int), 0666|IPC_CREAT);
-	if (shmid < 0) return 1;
-	if ((seg = shmat(shmid, 0, 0)) == NULL)	return 1;
+	TRY(shmid < 0)
+	TRY((seg = shmat(shmid, 0, 0)) == NULL)
 	f = (futex_t*)seg;
 	val = (int*)seg + sizeof(futex_t);
 
-	if (futex_init(f)) return 1;
-	if (futex_lock(f)) return 1;
+	TRY(futex_init(f))
+	TRY(futex_lock(f))
 
 	for (j = 0; j < PROCNUM; ++j) {
-		if ((pid = fork()) < 0) return 1;
+		TRY((pid = fork()) < 0)
 		if (pid == 0) {
 			for (i = 0; i < CYCLES; ++i)
 			{
-				if (futex_lock(f)) return 1;
+				TRY(futex_lock(f))
 				temp = *val;
 				temp++;
 				*val = temp;
-				if (futex_unlock(f)) return 1;
+				TRY(futex_unlock(f))
 			}
 			return 0;
 		}
 	}
-	if (futex_unlock(f)) return 1;
+	TRY(futex_unlock(f))
 
 	for (j = 0; j < PROCNUM; ++j) {
 		wait(NULL);
@@ -49,11 +51,11 @@ int main(int argc, char** argv)
 	temp = *val;
 	printf("Value: %d\n", temp);
 
-	if (futex_destroy(f)) return 1;
+	TRY(futex_destroy(f))
 
-	if (shmdt(seg)) return 1;
-	if (shmctl(shmid, IPC_RMID, 0)) return 1;
-	if (temp != CYCLES * PROCNUM) return 1;
+	TRY(shmdt(seg))
+	TRY(shmctl(shmid, IPC_RMID, 0))
+	TRY(temp != CYCLES * PROCNUM)
 	return 0;
 }
 
